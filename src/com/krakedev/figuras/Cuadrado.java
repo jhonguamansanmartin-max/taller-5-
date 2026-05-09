cc79f337be2521f681324e3192fa2ec244ce3f13
jhonguamansanmartin-max <jhonguamansamartin@gmail.com>
2026-05-09 06:04:00 -0500

    clase cuadrado

--- src/com/krakedev/figuras/Cuadrado.java
+++ src/com/krakedev/figuras/Cuadrado.java
@@ -1,5 +1,20 @@
 package com.krakedev.figuras;
 
-public class Cuadrado {
+public class Cuadrado extends Figura {
+    private int lado;
 
+    public Cuadrado(String nombre, String color, int lado) {
+        super(nombre, color);
+        this.lado = lado;
+    }
+
+    @Override
+    public int calcularPerimetro() {
+        return 4 * lado;
+    }
+
+    @Override
+    public double calcularArea() {
+        return lado * lado;
+    }
 }
